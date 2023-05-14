7466075ed16b29a7382cdc8910a722df43b971fa
Imran Gadzhiev <i.gadzhiev.m@gmail.com>
2023-05-14 07:00:02 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+  const [state, setState] = useState(null);
+  const [loading, setLoading] = useState('idle')
+  const [button, setButton] = useState(false)
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/languages");
+        const result = await response.json();
+        console.log(result, "result")
+        setState(result);
+        setLoading('success')
+      } catch (e) {
+        setLoading('error')
+        console.error(e.message, "error in request");
+      }
+    };
+    
+    if (button) {
+      setLoading('pending')
+      fetchData();
+    }
+  }, [button]);
+  
+  const handleButton = () => {
+    setButton(!button)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleButton}>start load langs: {String(button)}</button>
+  
+      <div>
+        {
+          loading === 'idle' ? 'state is null' : null
+        }
+      {
+        loading === 'error' ? "error" : null
+      }
+  
+      {
+        loading === 'pending' ? '...' : null
+      }
+  
+        {
+          loading === 'success' ? state.match(item => (
+            <div>{item}</div>
+          )) : null
+        }
+      </div>
     </div>
   );
 }
